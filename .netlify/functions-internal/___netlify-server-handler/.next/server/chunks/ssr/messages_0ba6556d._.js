61f06bd5b450b8c59852a7729c1c34dbdaab462c
Behar K <info@behark.com>
2025-09-09 18:22:55 +0200

    improvements

--- .netlify/functions-internal/___netlify-server-handler/.next/server/chunks/ssr/messages_0ba6556d._.js
+++
@@ -1,3 +0,0 @@
-module.exports=[65575,a=>{a.v(b=>Promise.all(["server/chunks/ssr/messages_de_json_56e8cde5._.js"].map(b=>a.l(b))).then(()=>b(63974)))},17741,a=>{a.v(b=>Promise.all(["server/chunks/ssr/messages_en_json_a7997480._.js"].map(b=>a.l(b))).then(()=>b(50929)))}];
-
-//# sourceMappingURL=messages_0ba6556d._.js.map
